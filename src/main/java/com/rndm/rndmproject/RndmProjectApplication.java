3e5cd05575e0a8a339b81f2c6d75ab1c918c9025
Narcis Gomez <narcisgomezquintana@gmail.com>
2019-10-15 13:21:22 +0200

    Basic structure
    Basic layer structure & some dependencies added

---
+++ src/main/java/com/rndm/rndmproject/RndmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rndm.rndmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RndmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RndmProjectApplication.class, args);
+    }
+
+}
