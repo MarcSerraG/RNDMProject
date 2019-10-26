65a8d464989114871f8824afb78599fa8f432419
David Pina <dpina@edu.tecnocampus.cat>
2019-10-26 12:39:50 +0200

    ThreadDAO getThread, getAuthor, SQL

--- src/main/java/com/rndm/rndmproject/persistence/ThreadDAO.java
+++ src/main/java/com/rndm/rndmproject/persistence/ThreadDAO.java
@@ -2,7 +2,10 @@ package com.rndm.rndmproject.persistence;
 
 import com.rndm.rndmproject.domain.Category;
 import com.rndm.rndmproject.domain.Thread;
+import com.rndm.rndmproject.domain.User;
+import com.rndm.rndmproject.persistence.UserDAO;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
@@ -17,8 +20,10 @@ public class ThreadDAO {
             "(id_thread, title, content, image_url, is_private, users_username, category_name) " +
             "values (?,?,?,?,?,?,?)";
     private final String NUM_THREADS = "select count(*) from thread where users_username = ?";
+    private final String GET_AUTHOR = "select users_username from thread where id_thread = ?";
+    private final String FIND_THREAD = "select * from thread where id_thread = ?";
 
-    //TODO ROWMAPPER
+    //TODO
     private Thread threadMapper(ResultSet resultSet) throws SQLException {
 
         Thread thread = new Thread(resultSet.getString("id"),
@@ -33,6 +38,24 @@ public class ThreadDAO {
 
         return thread;
     };
+
+    private final RowMapper<Thread> mapper = (resultSet, i) -> {
+        return threadMapper(resultSet);
+    };
+
+    public ThreadDAO(JdbcTemplate jdbctemplate){
+        this.jdbctemplate = jdbctemplate;
+    }
+
+    public Thread getThread(String id){
+        return jdbctemplate.queryForObject(FIND_THREAD, mapper, id);
+    }
+
+    /* Returns Author's username */
+    public String getAuthor(String id) {
+        return this.jdbctemplate.queryForObject(GET_AUTHOR, String.class, id);
+    }
+
 
 
 }
